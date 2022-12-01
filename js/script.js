1acca1f69395443b39450d900845a5602805ef90
Ismael-devTR <dev.tristan.romero@gmail.com>
2022-11-30 18:38:29 -0600

    feature/vhapter55

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 let pet_atackSection = document.getElementById("pet-atack");
 let petSelection = document.getElementById("btn-pet-selection");
-let btn_fuego = document.getElementById("btn-fuego");
-let btn_agua = document.getElementById("btn-agua");
-let btn_tierra = document.getElementById("btn-tierra");
+let btn_fuego;
+let btn_agua;
+let btn_tierra;
 let btn_restart = document.getElementById("btn-restart");
 let lifes_player = document.getElementById("lifes-player");
 let lifes_enemy = document.getElementById("lifes-enemy");
@@ -10,19 +10,23 @@ let ataquesJugador = document.getElementById("ataquesJugador");
 let ataquesEnemigo = document.getElementById("ataquesEnemigo");
 let mensajes = document.getElementById("resultado");
 let seccionSeleccionMascota = document.getElementById("pet-selection");
-let input_Hipodoge = document.getElementById("Hipodoge");
-let input_Capipepo = document.getElementById("Capipepo");
-let input_Ratigueya = document.getElementById("Ratigueya");
+let input_Hipodoge;
+let input_Capipepo;
+let input_Ratigueya;
 let input_Langostelvis = document.getElementById("Langostelvis");
 let input_Tucapalma = document.getElementById("Tucapalma");
 let input_Pydos = document.getElementById("Pydos");
 let mascotaNameEnemy = document.getElementById("mascotaNameEnemy");
+let mascotas_card = document.getElementById("mascotas'card");
+let groupAtack = document.getElementById("groupAtack");
 
 let mokepones = [];
 let ataqueJugador;
 let ataqueEnemigoPlayer;
 let lifesPlayer = 3;
 let lifesEnemy = 3;
+let opcionesmokepones;
+let mascotaJugador;
 
 class Mokepon {
   constructor(nombre, foto, vida) {
@@ -52,26 +56,47 @@ let Ratigueya = new Mokepon(
 );
 
 Hipodoge.ataques.push(
-  { nombre: "Agua", id: "" },
-  { nombre: "Agua", id: "" },
-  { nombre: "Agua", id: "" }
+  { nombre: "💧", id: "btn-agua" },
+  { nombre: "🔥", id: "btn-fuego" },
+  { nombre: "🌱", id: "btn-tierra" }
+);
+
+Capipepo.ataques.push(
+  { nombre: "💧", id: "btn-agua" },
+  { nombre: "🔥", id: "btn-fuego" },
+  { nombre: "🌱", id: "btn-tierra" }
+);
+
+Ratigueya.ataques.push(
+  { nombre: "💧", id: "btn-agua" },
+  { nombre: "🔥", id: "btn-fuego" },
+  { nombre: "🌱", id: "btn-tierra" }
 );
 
 mokepones.push(Hipodoge, Capipepo, Ratigueya);
 
-console.log(mokepones);
-
 function iniciarJuego() {
   pet_atackSection.style.display = "none";
   btn_restart.style.display = "none";
+
+  mokepones.forEach((mokepon) => {
+    opcionesmokepones = ` 
+    <input type="radio" name="mascota" id=${mokepon.nombre} />
+    <label class="tarjeta-mokepon" for=${mokepon.nombre} >
+      <p>${mokepon.nombre}</p>
+      <img src='${mokepon.foto}' alt=${mokepon.nombre} />
+    </label>`;
+    mascotas_card.innerHTML += opcionesmokepones;
+    input_Hipodoge = document.getElementById("Hipodoge");
+    input_Capipepo = document.getElementById("Capipepo");
+    input_Ratigueya = document.getElementById("Ratigueya");
+  });
+
   petSelection.addEventListener("click", () => {
     seleccionarMascotaJugador();
     pet_atackSection.style.display = "flex";
   });
 
-  btn_fuego.addEventListener("click", ataqueFuego);
-  btn_agua.addEventListener("click", ataqueAgua);
-  btn_tierra.addEventListener("click", ataqueTierra);
   btn_restart.addEventListener("click", restartGame);
 }
 
@@ -91,14 +116,9 @@ function ataqueTierra() {
 }
 
 function ataqueEnemigo() {
-  let ataque = aleatorio(1, 3);
-  if (ataque == 1) {
-    ataqueEnemigoPlayer = "FUEGO";
-  } else if (ataque == 2) {
-    ataqueEnemigoPlayer = "AGUA";
-  } else if (ataque == 3) {
-    ataqueEnemigoPlayer = "TIERRA";
-  }
+  let ataque = aleatorio(0, mokepones.length - 1);
+
+  ataqueEnemigoPlayer.innerHTML(mokepones[ataque].nombre);
   combate();
 }
 
@@ -164,23 +184,54 @@ function crearElementoFinal(texto) {
 function seleccionarMascotaJugador() {
   if (input_Hipodoge.checked) {
     mascotaNamePlayer.innerHTML = "Hipodoge";
+    mascotaJugador = input_Hipodoge.id;
   } else if (input_Capipepo.checked) {
     mascotaNamePlayer.innerHTML = "Capipepo";
+    mascotaJugador = input_Capipepo.id;
   } else if (input_Ratigueya.checked) {
     mascotaNamePlayer.innerHTML = "Ratigueya";
+    mascotaJugador = input_Ratigueya.id;
   } else if (input_Langostelvis.checked) {
     mascotaNamePlayer.innerHTML = "Langostelvis";
+    mascotaJugador = input_Langostelvis.id;
   } else if (input_Tucapalma.checked) {
     mascotaNamePlayer.innerHTML = "Tucapalma";
+    mascotaJugador = input_Tucapalma.id;
   } else if (input_Pydos.checked) {
     mascotaNamePlayer.innerHTML = "Pydos";
+    mascotaJugador = input_Pydos.id;
   } else {
     alert("Mascota no seleccionada");
     return;
   }
 
+  extraerAtaques(mascotaJugador);
   seleccionarMascotaEnemigo();
   seccionSeleccionMascota.style.display = "none";
+}
+
+function extraerAtaques(mascotaJugador) {
+  let ataques;
+  mokepones.forEach((mokepon) => {
+    mascotaJugador === mokepon.nombre && (ataques = mokepon.ataques);
+  });
+  console.log(ataques);
+  mostrarAtaques(ataques);
+}
+function mostrarAtaques(ataques) {
+  let ataques_moqkepones;
+  ataques.forEach((ataque) => {
+    ataques_moqkepones = `
+    <button id=${ataque.id} class="btn-at">${ataque.nombre}</button>`;
+    groupAtack.innerHTML += ataques_moqkepones;
+  });
+  btn_fuego = document.getElementById("btn-fuego");
+  btn_agua = document.getElementById("btn-agua");
+  btn_tierra = document.getElementById("btn-tierra");
+
+  btn_fuego.addEventListener("click", ataqueFuego);
+  btn_agua.addEventListener("click", ataqueAgua);
+  btn_tierra.addEventListener("click", ataqueTierra);
 }
 
 function seleccionarMascotaEnemigo() {
